26983b3353c6cded57f6898d6196b83fa93d548c
CTHall05 <63585450+CTHall05@users.noreply.github.com>
2023-07-15 12:33:30 +1000

    Test Commit

---
+++ employee-tracker-crud/backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
+// // const userRoutes = require('./routes/users');
+// app.use('/users', userRoutes);
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
